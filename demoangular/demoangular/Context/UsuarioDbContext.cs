f5630c6c09a2f4b4f64d06397c84775514d5b56f
Eschechola <lucas.eschechola@outlook.com>
2019-12-06 00:25:59 -0300

    Adicionando Entity Framework e banco SQLServer

---
+++ demoangular/demoangular/Context/UsuarioDbContext.cs
@@ -0,0 +1,13 @@
+﻿using demoangular.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoangular.Context
+{
+    public class UsuarioDbContext : DbContext
+    {
+        public UsuarioDbContext(DbContextOptions<UsuarioDbContext> options) : base(options)
+        {}
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
