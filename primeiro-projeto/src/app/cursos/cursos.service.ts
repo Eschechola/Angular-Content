4518f8bf73e92f0688f7973cc721e0c6cfd7b663
Eschechola <lucas.eschechola@outlook.com>
2020-01-09 23:41:06 -0300

    Aula 07

---
+++ primeiro-projeto/src/app/cursos/cursos.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class CursosService {
+
+  constructor() { }
+
+  getCursos(){
+    return ['C#', 'Angular', 'Entity Framework']
+  }
+}
