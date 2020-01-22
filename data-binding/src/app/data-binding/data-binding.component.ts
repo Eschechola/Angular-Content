f972442483c0e657968721593059faf291c44f3c
Eschechola <lucas.eschechola@outlook.com>
2020-01-22 00:28:32 -0300

    Aula 11 - Event Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -27,6 +27,49 @@ export class DataBindingComponent implements OnInit {
   urlImagem: string = 'https://scontent.fcgh12-1.fna.fbcdn.net/v/t1.0-9/p720x720/57311501_323213241698452_8222770121471426560_o.jpg?_nc_cat=102&_nc_ohc=_uz2VyN4gfkAX8av577&_nc_ht=scontent.fcgh12-1.fna&_nc_tp=1002&oh=2f6ce626521d6ba88f1a29e1f6d69593&oe=5EA97ABE'
 
 
+  /* EVENT BINDING */
+  botaoClicado(){
+    alert('Clicado')
+  }
+
+  valorAtual: string = "";
+  
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+    
+  }
+
+  // OU PODE TER O INTELI SENSE DO EVENTO
+
+  
+
+  onKeyUp2(evento){
+    alert(evento.target.value);
+  }
+
+
+  valorSalvo: string = ""; 
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+
+  valorBlur: string = "";
+
+  salvarBlur(valor){
+    this.valorBlur = valor;
+  }
+
+
+  onMouseOver(){
+    alert('Mouse em cima');
+  }
+
+  onMouseOut(){
+    alert('Mouse removido');
+  }
+
   constructor() { }
 
   ngOnInit() {
